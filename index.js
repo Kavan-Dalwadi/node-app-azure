91651953f3da9db8aeca3b03b7d5b2f40d6d5db0
Kavan Dalwadi <48027198+Kavan-Dalwadi@users.noreply.github.com>
2023-01-16 12:05:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -7,7 +13,6 @@ const server = http.createServer((req, res) => {
   res.end(msg);
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
- 
+app.get("/get-api", function(req, res) {
+    res.status(200).send("Hello World!");
+  });
